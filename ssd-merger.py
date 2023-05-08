90704be07c2396d0540f7443db245ced7f4790f6
Julian Schneider <schneider@zbmed.de>
2023-05-08 14:38:38 +0200

    (Parameter distribution) code to produce QTT file and run it

--- ssd-merger.py
+++ ssd-merger.py
@@ -2,6 +2,7 @@ import subprocess
 import os
 import wget
 from template_creators.process import process_template
+from template_creators.parameter_distribution import param_dist_template
 
 
 robot_installation = '/custom/path/to/ROBOT'
@@ -11,9 +12,9 @@ os.environ['PATH'] += ':'+robot_installation
 if not os.path.isfile('bfo.owl'):
     wget.download('http://purl.obolibrary.org/obo/bfo.owl')
 
-qtt_file = process_template()
+qtt_files = [process_template(), param_dist_template()]
 
-robot_cmd = 'robot template --merge-before -t '+qtt_file+' --input bfo.owl --output bfo_with_ssd.owl'
+robot_cmd = 'robot template --merge-before -t '+ ' -t '.join(qtt_files)+' --input bfo.owl --output bfo_with_ssd.owl'
 subprocess.run(robot_cmd, shell=True)
 
 print('fin')
