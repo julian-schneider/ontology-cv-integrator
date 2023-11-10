4d2eae63f1416f01a1d18ace4ea6be9cc703a1fd
Julian Schneider <schneider@zbmed.de>
2023-11-10 11:58:29 +0100

    Implement handling of the new type and language params

--- qtt-creator.py
+++ qtt-creator.py
@@ -36,28 +36,38 @@ for qtt_def in qtt_defs:
     df_in = pd.read_excel(qtt_def['file'], sheet_name=qtt_def['sheet'], keep_default_na=False)
     df_in.drop([idx-2 for idx in qtt_def['drop_rows']], inplace=True) #shift drop-indices because it is given as excel-index
 
+    # entities are either classes, or instances of parent Class. TODO support properties
+    entity_type = 'owl:Class' if qtt_def['type'] == 'class' else qtt_def['parent_iri']
     qtt_def['template_cols'].append({ # automatically define type column
         "name": "entity type (automatic)",
         "column": None,
         "template": "TYPE",
         "transformations": [{
             "type": "use_fixed",
-            "params": [qtt_def['parent_iri']]
+            "params": [entity_type]
         }]
       })
+
+    # if entities are classes, superclass is the parent class. If they are individuals, there is no superclass.
+    superclass = qtt_def['parent_iri'] if qtt_def['type'] == 'class' else ''
     qtt_def['template_cols'].append({ # automatically define superclass column
         "name": "superclass (automatic)",
         "column": None,
         "template": "SC %",
         "transformations": [{
             "type": "use_fixed",
-            "params": ['']
+            "params": [superclass]
         }]
       })
 
     finished_qtt_cols = []
     for template_def in qtt_def['template_cols']:
         if 'name' not in template_def: template_def['name'] = 'UNNAMED'
+
+        # If language is given for the qtt, LABEL gets the annotation
+        template_str = template_def['template']
+        if 'language' in qtt_def and template_str == 'LABEL':
+            template_str = 'AL rdfs:label@'+qtt_def['language']
 
         if template_def['template'] == 'ID': parent_cell = qtt_def['parent_iri']
         elif template_def['template'] == 'LABEL': parent_cell = qtt_def['parent_label']
@@ -68,11 +78,11 @@ for qtt_def in qtt_defs:
         transformed_col = pd.Series('', index=range(df_in.shape[0])) if template_def['column'] is None else df_in[template_def['column']]
         for trans_def in template_def['transformations']:
             transformed_col = transformed_col.apply(apply_transformation, args=(trans_def['type'], trans_def['params']))
-        finished_qtt_cols.append(pd.concat([pd.Series([template_def['name'], template_def['template'], parent_cell]), transformed_col], ignore_index=True))
+        finished_qtt_cols.append(pd.concat([pd.Series([template_def['name'], template_str, parent_cell]), transformed_col], ignore_index=True))
 
     df_out = pd.concat(finished_qtt_cols, axis=1, keys=[s.name for s in finished_qtt_cols])
     template_row = df_out.loc[1]
-    assert 'ID' in set(template_row) and 'LABEL' in set(template_row), 'The QTT definition for '+qtt_def['qtt_name']+' must contain "ID" and "LABEL" templates'
+    assert 'ID' in set(template_row) or 'LABEL' in set(template_row), 'The QTT definition for '+qtt_def['qtt_name']+' must contain "ID" or "LABEL" templates'
     df_out.to_csv('qtt/'+qtt_def['qtt_name'], sep='\t', header=False, index=False)
     finished_qtt_files.append('qtt/'+qtt_def['qtt_name'])
 
